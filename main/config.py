94f7b2657c44b2b5d55fa9d5cddd0558ab9295c2
sanjarbek-ai <sanjarbekmohinaxon0205@gmail.com>
2022-01-27 10:42:39 +0500

    initial

---
+++ main/config.py
@@ -0,0 +1,16 @@
+from environs import Env
+
+env = Env()
+env.read_env()
+
+BOT_TOKEN = env.str("BOT_TOKEN")
+ADMINS = env.list("ADMINS")
+
+DB_USER = env.str("DB_USER")
+DB_PASS = env.str("DB_PASS")
+DB_HOST = env.str("DB_HOST")
+DB_PORT = env.str("DB_PORT")
+DB_NAME = env.str("DB_NAME")
+
+I18N_DOMAIN = 'costs'
+LOCALES_DIR = 'locales'
